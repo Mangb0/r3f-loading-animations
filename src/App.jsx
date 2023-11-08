966c28c071c68a763037fe7058d3698512d94495
Mangb0 <whdudghks45629@gmail.com>
2023-11-09 00:33:02 +0900

    Add Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Suspense } from "react";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
   return (
     <>
       <Canvas shadows camera={{ position: [-5, 1, 6], fov: 25 }}>
         <fog attach="fog" args={["#16a04b", 12, 30]} />
-        <Experience />
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense>
         <EffectComposer>
           <Bloom
             mipmapBlur
@@ -17,6 +21,7 @@ function App() {
           />
         </EffectComposer>
       </Canvas>
+      <LoadingScreen />
     </>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,3 +1,21 @@
+import { useProgress } from "@react-three/drei";
+
 export const LoadingScreen = () => {
-  return <div></div>;
+  const { progress } = useProgress();
+  return (
+    <div className={"loadingScreen"}>
+      <div className="loadingScreen_progress">
+        <div
+          className="loadingScreen_progress_value"
+          style={{ width: `${progress * 100}%` }}
+        />
+      </div>
+      <div className="loadingScreen_board">
+        <h1 className="loadingScreen_title">Please help me!</h1>
+        <button className="loadingScreen_button" disabled={progress < 100}>
+          Start
+        </button>
+      </div>
+    </div>
+  );
 };
