7c45909fd443040e0b12d47cf2dc6140c2a689cc
Mangb0 <whdudghks45629@gmail.com>
2023-11-09 22:04:45 +0900

    Add Loading Screen with Audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 
+const audio = new Audio("./audios/Song Of Unity.mp3");
+
 function App() {
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (start) {
+      audio.play();
+    }
+  }, [start]);
+
   return (
     <>
       <Canvas shadows camera={{ position: [-5, 1, 6], fov: 25 }}>
         <fog attach="fog" args={["#16a04b", 12, 30]} />
-        <Suspense fallback={null}>
-          <Experience />
-        </Suspense>
+        <Suspense fallback={null}>{start && <Experience />}</Suspense>
         <EffectComposer>
           <Bloom
             mipmapBlur
@@ -21,7 +29,7 @@ function App() {
           />
         </EffectComposer>
       </Canvas>
-      <LoadingScreen />
+      <LoadingScreen started={start} onStarted={() => setStart(true)} />
     </>
   );
 }
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,18 +1,22 @@
 import { useProgress } from "@react-three/drei";
 
-export const LoadingScreen = () => {
+export const LoadingScreen = ({ started, onStarted }) => {
   const { progress } = useProgress();
   return (
-    <div className={"loadingScreen"}>
+    <div className={`loadingScreen ${started ? "loadingScreen--started" : ""}`}>
       <div className="loadingScreen_progress">
         <div
-          className="loadingScreen_progress_value"
-          style={{ width: `${progress * 100}%` }}
+          className="loadingScreen__progress__value"
+          style={{ width: `${progress}%` }}
         />
       </div>
-      <div className="loadingScreen_board">
-        <h1 className="loadingScreen_title">Please help me!</h1>
-        <button className="loadingScreen_button" disabled={progress < 100}>
+      <div className="loadingScreen__board">
+        <h1 className="loadingScreen__title">Please help me!</h1>
+        <button
+          className="loadingScreen__button"
+          disabled={progress < 100}
+          onClick={onStarted}
+        >
           Start
         </button>
       </div>
